6db7e77df6807bbe727e7d4f9656ebff944f0a0e
harshit29062006 <harshit29062006@gmail.com>
2025-08-26 18:35:20 +0530

    Add files via upload

---
+++ Assignment 2/ques_7.cpp
@@ -0,0 +1,25 @@
+/*7) Let A[1 …. n] be an array of n real numbers. A pair (A[i], A[j ]) is said to be an
+inversion if these numbers are out of order, i.e., i < j but A[i]>A[j ]. Write a program to
+count the number of inversions in an array*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) 
+        cin >> arr[i];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                count++;
+            }
+        }
+    }
+
+    cout << "Number of inversions: " << count << endl;
+    return 0;
+}
